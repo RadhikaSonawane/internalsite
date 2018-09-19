d89a2e46ea26b56eda1fdc65bd75755a5dc8e373
RadhikaSonawane <resultify@Kristians-iMac.local>
2018-09-19 10:39:18 +0200

    [TASK]set single.articles and flexible content ACF

--- web/app/themes/internalsite/flexible-content.php
+++ web/app/themes/internalsite/flexible-content.php
@@ -1,62 +1,62 @@
-<div class="container">
-    <div class="row">
-        <?php 
-        $name = get_field('name');
-        $content = get_field('content');
-        $text = get_field('text');
-        $text2 = get_field('text_2');
-        $image = get_field('image');
-        $relevant_gallerys = get_field('relevant_gallery');
-        $description = get_field('description');?>
 
-        <div class="col-6">
-                <p><?php echo $name; ?></p><br/>
-        </div>
-        
+<?php    
+$title             = get_field('title');
+$content           = get_field('content');
+$text              = get_field('text');
+$text2             = get_field('text_2');
+$image             = get_field('image');
+$relevant_gallerys = get_field('relevant_gallery');
+$description       = get_field('description')
+?>
+
+<div class="container">
+    <h1 class="my-4 text-center text">
+        <?php echo $title; ?>
+    </h1>
+
+    <div class="row">
         <div class="col-12 " >
             <img class="img-fluid" alt="Responsive image" src="<?php echo $image; ?>">
         </div>
-     <div class="container">
-        <div class="row">
-            <div class="col-sm pics">
-                            <div class="repeater">
-                                <?php
 
-                                    // check if the repeater field has rows of data
-                                    if( have_rows('relevant_gallery') ):
-
-                                    // loop through the rows of data
-                                    while ( have_rows('relevant_gallery') ) : the_row();
-                                ?>
-
-                                    <!-- display a sub field value-->
-                                        <img src="<?php echo the_sub_field('photos'); ?>" alt="">
-                                    <?php 
-                                        endwhile;
-
-                                        else :
-
-                                        // no rows found
-
-                                        endif;
-
-                                    ?>
-                            </div>
-                </div>  
+        <div class="col-sm pics">
+            <div class="repeater">
+                <?php
+                // check if the repeater field has rows of data
+                if( have_rows('relevant_gallery') ):
+                    // loop through the rows of data
+                    while ( have_rows('relevant_gallery') ) : the_row();
+                        ?>
+                        <!-- display a sub field value-->
+                        <img src="<?php echo the_sub_field('photos'); ?>" alt="">
+                        <?php 
+                    endwhile;
+                    else :
+                    // no rows found
+                endif;
+                ?>
+            </div>
         </div>
+    </div> 
+    
+    <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <p>
+                <?php echo $text; ?>
+            </p>
         </div>
-            
-            <div class="col-sm ">
-                <p><?php echo $text; ?></p><br/>
-            </div>
 
-            <div class="col-sm ">
-                <p><?php echo $text2; ?></p><br/>
-            </div>
-            
-            <div class="col-12">
-                <p><?php echo $description; ?></p><br/>
-            </div>
-        
-    </div><!--end row-->
-  </div><!--end container-->
+        <div class="col-sm-12 col-md-6 col-lg-6">
+            <p>
+                <?php echo $text2; ?>
+            </p>
+        </div>
+
+        <div class="col-12">
+            <p>
+                <?php echo $description; ?>
+            </p>
+        </div>
+    </div>
+
+</div><!--end container-->
--- web/app/themes/internalsite/single-article.php
+++ web/app/themes/internalsite/single-article.php
@@ -1,68 +1,33 @@
-<?php get_header();?>
+<?php   get_header();?>
 
 <div class="container">
-    <div class="row">
-        <div class="row align-items-end">
-            <?php if (have_posts()) { ?>
-            <?php while (have_posts()) { the_post() ?>
-            <div class="card-title test" style="font-size: 18px;">
-                <h1><?php the_title();?></h1>
-            </div> 
+    <?php 
+ 
+    get_template_part('flexible-content', 'flexible-content'); 
+    get_template_part('comment', 'comment');/*Add Comment section*/ 
+    $postid = get_the_ID();        
+    $args = array('post_id' => $postid,);
+    ?>
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-12">
+                <?php
+                $comments = get_comments($args);?>
+                    <div class="post-description">     
+                        <?php
+                        foreach ($comments as $comment) {
+                        echo "<div class='col-md-12 card' style='margin:5px; background-color:#f5f2ed;'>";
+                        echo $comment->comment_author;
+                        echo "<br>";
+                        echo $comment->comment_content;
+                        echo "<br>";
+                        echo $comment->comment_date;
+                        echo "<br/>";
+                        echo "</div>";
+                        };
+                        ?>
+                    </div><!--/post-description-->
+            </div><!--/col-sm-8-->
+        </div>
+</div>
 
-            <div class="card-img-top" >
-                <?php if ( has_post_thumbnail() ) { ?>
-                <img class="card-img-top" src="<?php the_post_thumbnail_url(); ?>" alt="Card image cap">
-                <?php } ?>  
-            </div>
-            <br/><br/>
-
-             <?php get_template_part('flexible-content', 'flexible-content');?> 
-
-             <!-- Add Content -->
-            <div class="card-text">
-                    <?php the_content(); ?> 
-                    <?php $postid = get_the_ID();?>
-                    
-
-             <!-- Add Comment section -->          
-             <?php get_template_part('comment', 'comment');?>
-                <?php $args = array(
-                    'post_id' => $postid,
-                );?>
-
-                        <div class= "cantainer">
-                                <div class="row">
-                                    <div class="col-sm-8">
-                                        <?php
-                                        $comments = get_comments($args);?>
-                                            <div class="post-description">     
-                                                <?php
-                                                foreach ($comments as $comment) {
-                                                    echo "<div class='col-md-12 card' style='margin:5px; background-color:#f5f2ed;'>";
-                                                    echo $comment->comment_author;
-                                                    echo "<br>";
-                                                    echo $comment->comment_content;
-                                                    echo "<br>";
-                                                    echo $comment->comment_date;
-                                                    echo "<br/>";
-                                                    echo "</div>";
-                                                };
-                                                ?>
-                                            </div><!--/post-description-->
-                                    </div><!--/col-sm-8-->
-                                </div><!--/row-->
-                        </div><!--/cantainer-->
-
-            <div class="card-text">
-                <?php } ?> <!--/while-->
-            <?php } else { ?>
-
-            Sorry We have no article that you are looking for!
-
-            <?php } ?> <!--/if-->
-            </div>
-        </div> <!--/col-md-9-->
-    </div>  <!--/row-->
-</div><!--/container-->
-
-<?php get_footer();?>
+    <?php get_footer();?>
