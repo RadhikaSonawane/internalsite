5e576ab5af09748e0271cf493cf4754dd532cb6b
Radhika <radhika.sonawane@resultify.se>
2018-10-19 15:08:55 +0200

    [TASK] make some css changes

--- web/app/plugins/woocommerce/includes/wc-template-functions.php
+++ web/app/plugins/woocommerce/includes/wc-template-functions.php
@@ -1066,7 +1066,7 @@ if ( ! function_exists( 'woocommerce_template_loop_product_title' ) ) {
 	 * Show the product title in the product loop. By default this is an H2.
 	 */
 	function woocommerce_template_loop_product_title() {
-		echo '<h2 class="woocommerce-loop-product__title">' . get_the_title() . '</h2>';
+		echo '<div class="title">' . get_the_title() . '</div>';
 	}
 }
 if ( ! function_exists( 'woocommerce_template_loop_category_title' ) ) {
